a6d8f013d9ca9af31a9ee9a77f7e53c98e1ee813
Frances Pasulohan <franpas12123@gmail.com>
2021-07-16 15:08:36 +0800

    Update App.js

--- client/device-inventory-app/src/App.js
+++ client/device-inventory-app/src/App.js
@@ -6,12 +6,14 @@ import Navbar from "./components/NavbarComponent"
 import AddDevice from "./components/AddDevice"
 import DevicesList from "./components/DevicesList"
 import EditDevice from "./components/EditDevice"
+// import Home from "./components/Home"
 
 function App() {
   return (
     <Router>
       <Navbar />
       <br />
+      {/* <Route path='/' component={Home} /> */}
       <Route path='/devices' component={DevicesList} />
       <Route path='/create' component={AddDevice} />
       <Route path='/edit/:id' component={EditDevice} />
