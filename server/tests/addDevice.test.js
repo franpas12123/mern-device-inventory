72bd16b609868af53c84acc20e4f50aa2c62757c
Frances D Pasulohan <franpas12123@gmail.com>
2021-07-17 15:48:41 +0800

    Create addDevice.test.js
    Initial test

---
+++ server/tests/addDevice.test.js
@@ -0,0 +1,18 @@
+const { addDevice } = require('../controllers/device')
+
+const BASE_URL = process.env.DEV_URL;
+
+test('adding new device', () => {
+    const name = "Test Name",
+        field = "Test Field",
+        desc = "Test Description",
+        availability = true
+    const newDevice = {
+        name: name,
+        field: field,
+        description: description,
+        availability: availability
+    }
+
+    expect(addDevice(req,res))
+})
