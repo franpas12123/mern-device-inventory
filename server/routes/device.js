5310a6b37742dad05cc146f393d63b8dc02bc7f0
Frances Pasulohan <franpas12123@gmail.com>
2021-07-20 08:20:49 +0800

    Update device.js
    Added getDevices to get all devices. Changed getDevice to get one device. Added updateDevice for updating

--- server/routes/device.js
+++ server/routes/device.js
@@ -1,9 +1,16 @@
 const Router = require('express').Router()
 const controller = require('../controllers/device')
 
-Router.route('/').get(controller.getDevice)
+
+Router.route('/health').get(controller.checkHealth)
+
+Router.route('/').get(controller.getDevices)
+
+Router.route('/:id').get(controller.getDevice)
 
 Router.route('/').post(controller.addDevice)
+
+Router.route('/:id').put(controller.updateDevice)
 
 Router.route('/:id').delete(controller.deleteDevice)
 
